48b5126f5ec28a5e92104ca093ec1e15f358cf3d
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-05-10 22:42:54 +0100

    need boutton, need func

--- calcul_moyenne.py
+++ calcul_moyenne.py
@@ -1,7 +1,10 @@
 import tkinter as tk
+from tkinter import messagebox
+
+def Calculer():
+    print("hiii")
 
 # Creation d'une fenetre 
-#nom_fenetre = tk.Tk()
 nom_fenetre = tk.Tk()
 # titre de fenetre
 nom_fenetre.title("App de calcul de moyenne : ")
@@ -9,20 +12,64 @@ nom_fenetre.title("App de calcul de moyenne : ")
 largeur = 600
 longueur = 400 
 nom_fenetre.geometry(f"{largeur}x{longueur}")
-
+# ---------- DONE ------
 #####################################################
-
+# TASK : 1 
 # AJOUTER UN CADRE
 nom_cadre = tk.Frame(nom_fenetre)
-nom_cadre.pack()
+nom_cadre.pack(pady=5)
 
 # AJOUTER LABEL
-nom_label = tk.Label(nom_fenetre, text = "Note 1 : ")
-nom_label.pack()
+nom_label = tk.Label(nom_cadre, text = "Note 1 : ")
+nom_label.pack(side = "left")
 
 # AJOUTER ZONE DE TEXTE
-nom_zone_texte = tk.Entry(nom_fenetre)
-nom_zone_texte.pack()
+nom_zone_texte = tk.Entry(nom_cadre) # kant nom_fenetre
+nom_zone_texte.pack(side = "left") 
+
+# AJOUTER BOUTTON 
+#nom_bouton = tk.Button(nom_cadre, text = "Calculer les moyennes", command=Calculer) # TODO : FIND OPTION N#3
+#nom_bouton.pack(pady=10, side=tk.BOTTOM) # TODO : fix me im waaay up
+
+# TASK 2 
+# AJOUTER UN CADRE
+nom_cadre2 = tk.Frame(nom_fenetre)
+nom_cadre2.pack(pady=5)
+
+# AJOUTER LABEL
+nom_label2 = tk.Label(nom_cadre2, text = "Note 2 : ")
+nom_label2.pack(side = "left")
+
+# AJOUTER ZONE DE TEXTE
+nom_zone_texte2 = tk.Entry(nom_cadre2) # kant nom_fenetre
+nom_zone_texte2.pack(side = "left") 
+
+# TASK : 3 
+# AJOUTER UN CADRE
+nom_cadre3 = tk.Frame(nom_fenetre)
+nom_cadre3.pack(pady=5)
+
+# AJOUTER LABEL
+nom_label3 = tk.Label(nom_cadre3, text = "Note 3 : ")
+nom_label3.pack(side = "left")
+
+# AJOUTER ZONE DE TEXTE
+nom_zone_texte3 = tk.Entry(nom_cadre3) # kant nom_fenetre
+nom_zone_texte3.pack(side = "left") 
+
+# TASK : 3 
+# AJOUTER UN CADRE
+nom_cadre_examen = tk.Frame(nom_fenetre)
+nom_cadre_examen.pack(pady=5)
+
+# AJOUTER LABEL
+nom_label_examen = tk.Label(nom_cadre_examen, text = "Examen : ")
+nom_label_examen.pack(side = "left")
+
+# AJOUTER ZONE DE TEXTE
+nom_zone_texte_examen = tk.Entry(nom_cadre_examen) # kant nom_fenetre
+nom_zone_texte_examen.pack(side = "left") 
+
 
 # start of fenetre
 nom_fenetre.mainloop()
@@ -31,4 +78,4 @@ nom_fenetre.mainloop()
 
 
 
-
+ 
